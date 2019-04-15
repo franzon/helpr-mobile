3b4729b22cdc84e68d7ee83fce467dcc77fdede8
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-04-15 14:17:51 -0300

    Separated folders

--- lib/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-final apiUrl = 'http://127.0.0.1:3000/api';
-final colors = {
-  "backgroundColor": Color(0xFF2B2E33),
-  "primaryColor": Color(0xFFF57474),
-  "accentColor": Colors.white
-};
