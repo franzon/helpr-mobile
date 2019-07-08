c5dae1a861f485fd160c26822778d24b6985d487
Everton <evertonabreu@alunos.utfpr.edu.br>
2019-07-08 15:11:29 -0300

    users info in drawer

--- client_v3/lib/pages/home/customDrawer.dart
+++ client_v3/lib/pages/home/customDrawer.dart
@@ -1,9 +1,41 @@
+import 'package:client_v3/models/Client.dart';
+import 'package:client_v3/repositories/ClientRepository.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:flutter_keychain/flutter_keychain.dart';
 import 'package:client_v3/constants/ui.dart';
 import 'package:client_v3/pages/authentication/Authentication.dart';
 
-class CustomDrawer extends StatelessWidget {
+import '../../setupSingletons.dart';
+
+class CustomDrawer extends StatefulWidget {
+  @override
+  _CustomDrawer createState() => _CustomDrawer();
+}
+
+class _CustomDrawer extends State<CustomDrawer> {
+  final _clientRepository = getIt<ClientRepository>();
+
+  @override
+  void initState() {
+  }
+
+  Future<void> signOut (context) async {
+    try{
+      await FlutterKeychain.remove(key: 'token');
+
+      Navigator.of(context).pushReplacement(
+      PageTransition(
+        child: AuthenticationPage(),
+        type: PageTransitionType.downToUp,
+      ),
+    );
+    }
+    catch(e) {
+
+    }
+    
+  }
 
   Widget build(BuildContext context) {
     return Theme(
@@ -15,12 +47,27 @@ class CustomDrawer extends StatelessWidget {
           children: <Widget>[
             UserAccountsDrawerHeader(
               decoration: BoxDecoration(color: colors["background"]),
-              accountName: Text("Ashish Rawat"),
-              accountEmail: Text("ashishrawat2911@gmail.com"),
+              accountName: FutureBuilder<Client>(
+                future: _clientRepository.getClient(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return Text( snapshot.hasData ?snapshot.data.name : "User name");
+                }
+              ),
+              accountEmail: FutureBuilder<Client>(
+                future: _clientRepository.getClient(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return Text( snapshot.hasData ? snapshot.data.email : "user@host.ccc");
+                }
+              ),
               currentAccountPicture: CircleAvatar(
                 backgroundColor: colors["background2"],
-                child: Text("A",
-                  style: TextStyle(fontSize: 40.0),
+                child: FutureBuilder<Client>(
+                  future: _clientRepository.getClient(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    return Text(snapshot.hasData ? snapshot.data.name[0].toUpperCase() : "A",
+                      style: TextStyle(fontSize: 40.0)
+                    );
+                  }
                 ),
               )
             ),
@@ -32,15 +79,9 @@ class CustomDrawer extends StatelessWidget {
                 ),
                 tooltip: 'Sign Out',
                 onPressed: () => {
-                  Navigator.of(context).pushReplacement(
-                    PageTransition(
-                      child: AuthenticationPage(),
-                      type: PageTransitionType.downToUp,
-                    ),
-                  )
-                }
-              )
-              
+                  this.signOut(context)
+                },
+              ),
             ),
           ],
         )
